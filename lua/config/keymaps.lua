ab8a48aecef612eb2e309e097f46f1c17ba192a7
Kyle M <kmacdonald76@gmail.com>
2025-03-10 10:34:14 -0700

    Remove some plugins, remap some keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- I don't use buffers.. bring back the 'move cursor to top/bottom of screen' behaviour
+vim.keymap.del("n", "<S-h>") -- Remove LazyVim's remap
+vim.keymap.del("n", "<S-l>") -- Remove LazyVim's remap
+
+vim.keymap.set("i", "jk", "<Esc>")
