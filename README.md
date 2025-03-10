6e1b1d223cd3f540f9725c8a0e29aba618d471c9
Kyle M <kmacdonald76@gmail.com>
2025-03-10 10:48:26 -0700

    Revive some keymaps, add instructions to readme

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+
+Instructions: 
+
+Pre-req: neovim
+
+1. ```bash
+      mv ~/.config/nvim ~/tmp/nvim_backup
+    ```
+    *(or delete it)*
+
+2. ```bash
+      git clone git@github.com:kmacdonald76/nvim-lazy-conf.git ~/.config/nvim
+    ```
+
+3. restart neovim or run `:Lazy sync`
+    ```
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- I don't use buffers.. bring back the 'move cursor to top/bottom of screen' behaviour
-vim.keymap.del("n", "<S-h>") -- Remove LazyVim's remap
-vim.keymap.del("n", "<S-l>") -- Remove LazyVim's remap
+-- I don't use buffers.. I use terminal tabs for each file.
+-- bring back the 'move cursor to top/bottom of screen' behaviour
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
 
+-- cannot figure out why 's' was remapped to search.. that's what slash is for.. delete it, so it can be used to substitute text
+vim.keymap.del("v", "s")
+
+-- remap escape key
 vim.keymap.set("i", "jk", "<Esc>")
